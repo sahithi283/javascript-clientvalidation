26f78122783411d15ed2a3c6529c25846cc20f38
Sahithi Kakkerla <Sahithi_Kakkerla@epam.com>
2019-07-15 17:07:07 +0530

    first commit

---
+++ validateemail.js
@@ -0,0 +1,20 @@
+function validateemail()  
+{  
+var x=document.myform.email.value;  
+var atposition=x.indexOf("@");  
+var dotposition=x.lastIndexOf(".");  
+if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+  alert("Please enter a valid e-mail address");  
+  return false;  
+  }   
+var firstpassword=document.myform.password.value;  
+var secondpassword=document.myform.password2.value;  
+  
+if(firstpassword==secondpassword){  
+return true;  
+}  
+else{  
+alert("password must be same!");  
+return false;  
+}  
+}
